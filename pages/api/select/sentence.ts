fddac1aaf8c21df3c470720e80d24dc8b7f64eb2
kyw0716 <kyw0716@naver.com>
2023-01-22 20:45:53 +0900

    fix: json 데이터 못 읽는 오류 수정 시도 ( api 내부에서 요청 진행 )

--- pages/api/select/sentence.ts
+++ pages/api/select/sentence.ts
@@ -1,17 +1,24 @@
+import { doc, getDoc } from "firebase/firestore";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getFirestoreDocData } from "../../../src/lib/firebaseUtils";
+import { FIREBASE_DB } from "../../../Firebase";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const sentences = await getFirestoreDocData("sentence", "sample");
+  const docRef = doc(FIREBASE_DB, "sentence", "sample");
+  const docSnap = await getDoc(docRef);
   const dataType = req.query.dataType as string;
 
-  if (req.method === "GET") {
-    if (dataType === "name")
-      return res.status(200).json(Object.keys(sentences));
-    if (dataType === undefined) return res.status(200).json(sentences);
-    return res.status(200).json(sentences[dataType]);
+  if (docSnap.exists()) {
+    const sentences = docSnap.data();
+
+    if (req.method === "GET") {
+      if (dataType === "name")
+        return res.status(200).json(Object.keys(sentences));
+      if (dataType === undefined) return res.status(200).json(sentences);
+      return res.status(200).json(sentences[dataType]);
+    }
   }
+  return res.status(404).json("not-found");
 }
