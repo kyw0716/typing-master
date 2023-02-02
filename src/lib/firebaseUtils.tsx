e3129ba446c7819ef4e552e9f80d46475d9a1b49
kyw0716 <kyw0716@naver.com>
2023-02-02 15:55:20 +0900

    fix: 문장별 유저 기록 등록 안되는 오류 수정

--- src/lib/firebaseUtils.tsx
+++ src/lib/firebaseUtils.tsx
@@ -47,15 +47,10 @@ export const addRecordToFirestore = async (
   // uid의 유저의 기록이 없을 때
   if (isEmpty(prevRecord[uid]))
     // sentenceId 문장에 다른 사람의 기록이 있으면 update, 아니면 set을 진행
-    await updateDoc(docRef, {
+    await setDoc(docRef, {
       [uid]: record,
-    }).catch(() =>
-      setDoc(docRef, {
-        [uid]: record,
-      })
-    );
-
-  if (prevSpeed < Number(record.speed) && Number(record.accuracy) >= 80)
+    });
+  else if (prevSpeed < Number(record.speed) && Number(record.accuracy) >= 80)
     await updateDoc(docRef, {
       [uid]: record,
     });
