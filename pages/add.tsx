21b9d3271b5450861c00afb918660898f27fb74a
kyw0716 <kyw0716@naver.com>
2023-02-04 17:53:56 +0900

    refactor: add 페이지 입력값 핸들링 방식 변경 useState => useReducer

--- pages/add.tsx
+++ pages/add.tsx
@@ -1,9 +1,8 @@
 import axios from "axios";
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useReducer, useRef } from "react";
 import styled from "styled-components";
 import { v4 } from "uuid";
 import Layout from "../src/components/layout";
-import useInput from "../src/lib/useInput";
 import { makeSentenceData } from "../src/lib/utils";
 import { userContext } from "./_app";
 
@@ -64,30 +63,70 @@ const Style = {
   `,
 };
 
+type InputValues = {
+  title: string;
+  content: string[];
+  isTitleExist: boolean;
+};
+
+type InputValueAction = {
+  title?: string;
+  content?: string;
+  isTitleExist?: boolean;
+  type: string;
+};
+
+const inputReducer = (
+  inputValues: InputValues,
+  inputAction: InputValueAction
+): InputValues => {
+  switch (inputAction.type) {
+    case "title":
+      if (inputAction.title !== undefined)
+        return {
+          ...inputValues,
+          title: inputAction.title,
+        };
+    case "content":
+      if (inputAction.content !== undefined)
+        return {
+          ...inputValues,
+          content: inputAction.content.split("\n").filter((v) => v !== ""),
+        };
+    case "titleExist":
+      if (inputAction.isTitleExist !== undefined)
+        return {
+          ...inputValues,
+          isTitleExist: inputAction.isTitleExist,
+        };
+  }
+  return inputValues;
+};
+
 export default function Add() {
-  const {
-    inputValue: title,
-    onChange: onChangeTitle,
-    resetInput: resetTitle,
-  } = useInput();
-
   const user = useContext(userContext);
-
-  const [content, setContent] = useState<string[]>([]);
-  const [isTitleExist, setIsTitleExist] = useState<boolean>(false);
+  const [inputValues, inputValuesDispatch] = useReducer(inputReducer, {
+    title: "",
+    content: [],
+    isTitleExist: false,
+  });
 
   const inputRef = useRef(null);
   const textAreaRef = useRef(null);
 
   useEffect(() => {
     if (inputRef.current) (inputRef.current as HTMLInputElement).focus();
-    if (isTitleExist && textAreaRef.current)
+    if (inputValues.isTitleExist && textAreaRef.current)
       (textAreaRef.current as HTMLTextAreaElement).focus();
-  }, [isTitleExist]);
+  }, [inputValues.isTitleExist]);
 
   useEffect(() => {
-    if (!title) setIsTitleExist(false);
-  }, [title]);
+    if (inputValues.title === "")
+      inputValuesDispatch({
+        type: "titleExist",
+        isTitleExist: false,
+      });
+  }, [inputValues.title]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -95,9 +134,13 @@ export default function Add() {
     if (!user?.uid) return alert("로그인 후 등록이 가능합니다!");
 
     const randomId = v4();
-    const data = makeSentenceData(title, content, randomId);
+    const data = makeSentenceData(
+      inputValues.title,
+      inputValues.content,
+      randomId
+    );
 
-    if (content.length > 0)
+    if (inputValues.content.length > 0)
       axios(`/api/addSentence`, {
         method: "POST",
         data: data,
@@ -108,12 +151,22 @@ export default function Add() {
         });
         handleAfterRequest();
       });
-    if (title.length > 0) setIsTitleExist(true);
+    if (inputValues.title.length > 0)
+      inputValuesDispatch({
+        type: "titleExist",
+        isTitleExist: true,
+      });
   };
 
   const handleAfterRequest = () => {
-    resetTitle();
-    setContent([]);
+    inputValuesDispatch({
+      type: "title",
+      title: "",
+    });
+    inputValuesDispatch({
+      type: "content",
+      content: "",
+    });
 
     if (textAreaRef.current)
       (textAreaRef.current as HTMLTextAreaElement).value = "";
@@ -121,42 +174,51 @@ export default function Add() {
     alert("등록이 완료되었습니다. 메뉴의 시작하기로 들어가서 확인해보세요!");
   };
 
+  const handleTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    inputValuesDispatch({
+      type: "title",
+      title: event.target.value,
+    });
+  };
+
+  const handleContent = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    inputValuesDispatch({
+      type: "content",
+      content: event.target.value,
+    });
+  };
+
   return (
     <Layout>
       <Style.Wrapper onSubmit={handleSubmit}>
         <Style.Container
           about="flex-start"
-          style={{ display: isTitleExist ? "none" : "flex" }}
+          style={{ display: inputValues.isTitleExist ? "none" : "flex" }}
         >
           <Style.Label htmlFor="TITLE">제목:</Style.Label>
           <Style.Input
             id="TITLE"
             placeholder="문장의 제목을 입력해주세요."
-            onChange={onChangeTitle}
-            value={title}
+            onChange={handleTitle}
+            value={inputValues.title}
             ref={inputRef}
           />
         </Style.Container>
         <Style.Container
           about="flex-start"
-          style={{ display: isTitleExist ? "flex" : "none" }}
+          style={{ display: inputValues.isTitleExist ? "flex" : "none" }}
         >
           <Style.Label htmlFor="CONTENT">문장:</Style.Label>
           <Style.TextArea
             id="CONTENT"
             placeholder="문장을 입력해주세요."
-            onChange={(event) => {
-              const sentences = event.target.value
-                .split("\n")
-                .filter((v) => v !== "");
-              setContent(sentences);
-            }}
+            onChange={handleContent}
             ref={textAreaRef}
-          ></Style.TextArea>
+          />
         </Style.Container>
         <Style.Container
           about="flex-end"
-          style={{ display: isTitleExist ? "flex" : "none" }}
+          style={{ display: inputValues.isTitleExist ? "flex" : "none" }}
         >
           <Style.SubmitButton type="submit" value="제출" />
         </Style.Container>
